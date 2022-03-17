cd73262c5b48a8d89ee191e2f4aa9a58c5a153de
Dirk Haage <dirk@mohawk.pro>
2022-03-18 00:11:09 +0100

    fix: hasOwnProperty for node 12/14

--- index.js
+++ index.js
@@ -204,7 +204,8 @@ function getAlpha2Code(name, lang) {
     const codenames = registeredLocales[lang.toLowerCase()];
     // eslint-disable-next-line no-restricted-syntax -- for loop for early exit
     for (p in codenames) {
-      if (Object.hasOwn(codenames, p)) {
+      // eslint-disable-next-line prefer-object-has-own -- keep for node@12 and node@14 support
+      if (Object.prototype.hasOwnProperty.call(codenames, p)) {
         if (codenames[p].toLowerCase() === name.toLowerCase()) {
           return p;
         }
